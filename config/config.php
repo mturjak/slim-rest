55d79dae1c05ddb130a45ef10f83ffaf0ef054cd
Martin Turjak <martinturjak@gmail.com>
2015-01-04 22:12:10 +0100

    added more comments to config.php

--- config/config.php
+++ config/config.php
@@ -7,6 +7,19 @@ ini_set("display_errors", 1);
 
 /**
  * Database configuration
+ * This is the place where you define your database credentials, type etc.
+ *
+ * database type
+ * define('DB_TYPE', 'mysql');
+ * database host, usually it's "127.0.0.1" or "localhost", some servers also need port info, like "127.0.0.1:8080"
+ * define('DB_HOST', '127.0.0.1');
+ * name of the database. please note: database and database table are not the same thing!
+ * define('DB_NAME', 'login');
+ * user for your database. the user needs to have rights for SELECT, UPDATE, DELETE and INSERT
+ * By the way, it's bad style to use "root", but for development it will work
+ * define('DB_USER', 'root');
+ * The password of the above user
+ * define('DB_PASS', 'xxx');
  */
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
